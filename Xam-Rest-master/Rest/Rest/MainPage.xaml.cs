c533cb6494aad5ff6dbd5a5e43a07a9f351dbdcf
oddo4 <oddo4@seznam.cz>
2018-06-01 12:41:59 +0200

    Created RecipeSearch class

--- Xam-Rest-master/Rest/Rest/MainPage.xaml.cs
+++ Xam-Rest-master/Rest/Rest/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using ModernHttpClient;
@@ -15,61 +16,21 @@ namespace Rest
 {
     public partial class MainPage : ContentPage
     {
+        RecipeSearchAPI api = new RecipeSearchAPI();
         public MainPage()
         {
             InitializeComponent();
-
+            
             ShowPosts();
         }
 
         public async void ShowPosts()
         {
-            string json = await GetPostsJsonTask();
-            var c = await ParsePostJsonTask(json);
-            
-        }
+            string json = await api.GetPostsJsonTask("chicken");
+            var c = await api.ParsePostJsonTask(json);
+            PostsListView.ItemsSource = c.Hits;
 
-        /// <summary>
-        /// Parse Json string via task - no lag on UI
-        /// </summary>
-        /// <param name="json">Json string of Posts</param>
-        /// <returns>Collection of parsed objects</returns>
-        /// <exception cref="HttpRequestException">If device could not connect ie. Internet access denied or Status code is not Success</exception>
-        public async Task<Welcome> ParsePostJsonTask(string json)
-        {
-            return await Task.Run(() => JsonConvert.DeserializeObject<Welcome>(json));
 
-        }
-
-        /// <summary>
-        /// Download data from API via background Task
-        /// </summary>
-        /// <returns>Json string from API</returns>
-        public async Task<string> GetPostsJsonTask()
-        {
-            var client = new HttpClient();
-            var uri = new Uri("https://api.edamam.com/search?q=chicken&app_id=df168dd4&app_key=a7d32a938a697834f1248f5973c4705c&from=0&to=3");
-
-            string content = await Task.Run(async () =>
-            {
-                var response = await client.GetAsync(uri);
-                if (response.IsSuccessStatusCode) return await response.Content.ReadAsStringAsync();
-                throw new HttpRequestException("Communication with server failed");
-            });
-            return content;
-        }
-
-        /// <summary>
-        /// GETs data from API in synchronous way, lags UI thread
-        /// Just for education purpose
-        /// </summary>
-        public void GetData()
-        {
-            HttpClient client = new HttpClient(new NativeMessageHandler());
-            var uri = new Uri("https://jsonplaceholder.typicode.com/posts/");
-
-            var response = client.GetAsync(uri).Result;
-            string content = response.Content.ReadAsStringAsync().Result;
         }
     }
 }
---
+++ Xam-Rest-master/Rest/Rest/Model/RecipeSearchAPI.cs
@@ -0,0 +1,55 @@
+﻿using ModernHttpClient;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rest.Model
+{
+    class RecipeSearchAPI
+    {
+        private string apiID = "df168dd4";
+        private string apiKey = "25df00d4dd3c62cd7caad4f4fb908673";
+
+        /// <summary>
+        /// Parse Json string via task - no lag on UI
+        /// </summary>
+        /// <param name="json">Json string of Posts</param>
+        /// <returns>Collection of parsed objects</returns>
+        /// <exception cref="HttpRequestException">If device could not connect ie. Internet access denied or Status code is not Success</exception>
+        public async Task<Welcome> ParsePostJsonTask(string json)
+        {
+            return await Task.Run(() => JsonConvert.DeserializeObject<Welcome>(json));
+
+        }
+
+        //Nutrition Analysis API and Database
+        //API ID = ef9393e8
+        //API KEY = e217c0120f0ee71b206806177c3e750e
+
+        //Recipe Search API
+        //API ID = df168dd4
+        //API KEY = a7d32a938a697834f1248f5973c4705c
+
+        /// <summary>
+        /// Download data from API via background Task
+        /// </summary>
+        /// <returns>Json string from API</returns>
+        public async Task<string> GetPostsJsonTask(string query)
+        {
+            var client = new HttpClient();
+            var uri = new Uri("https://api.edamam.com/search?q=" + query + "&app_id=" + apiID + "&app_key=" + apiKey + "&from=0&to=3");
+
+            string content = await Task.Run(async () =>
+            {
+                var response = await client.GetAsync(uri);
+                if (response.IsSuccessStatusCode) return await response.Content.ReadAsStringAsync();
+                throw new HttpRequestException("Communication with server failed");
+            });
+            return content;
+        }
+    }
+}
